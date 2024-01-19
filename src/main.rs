d45fab465694044e25c011b7226773ae289e79fb
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2024-01-19 23:12:01 +0100

    update array size

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
 
+// Implement the binary search algorithm.
 // Function to perform binary search on a sorted array.
 fn binary_search(
     result: Result<usize, usize>,
@@ -64,10 +65,10 @@ fn main() {
     }
 
     // Define a sorted array of integers.
-    let array1: [i32; 11] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-
+    let array1: [i32; 100] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+    
     // Specify the target integer to search for.
-    let target = 9;
+    let target = 45;
 
     // Perform binary search on the array to find the target.
     let result: Result<usize, usize> = array1.binary_search(&target);
